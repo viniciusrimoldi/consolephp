4d0dd35586b17b2900848a54568a32f1f0b77f26
viniciusrimoldi <30911840+viniciusrimoldi@users.noreply.github.com>
2020-12-22 10:58:41 -0900

    Update ls.php

--- bin/ls.php
+++ bin/ls.php
@@ -4,7 +4,7 @@
 
 $arg = $dir_user . $cmd[1] . '/*';  //path to list.
 $fileList = glob($arg);             //get a list of file paths with glob().
-$text = "\n";                       //variable to list results.
+$text = "";                         //variable to list results.
 
 foreach($fileList as $filename){                                //loop through the array that glob returned.
    $text = $text . str_replace($dir_user, "", $filename) . "  ";  //simply print them out onto the screen.
