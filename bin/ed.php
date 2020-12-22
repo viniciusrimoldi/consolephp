5128a05a9bc5999bc974c0d06b534625bdc79f94
viniciusrimoldi <30911840+viniciusrimoldi@users.noreply.github.com>
2020-12-22 13:14:23 -0900

    Add function to write logs.

--- bin/ed.php
+++ bin/ed.php
@@ -2,13 +2,23 @@
 // bin/ed.php
 // 202012201900
 
-// Date and time now.
+// Global variables.
 date_default_timezone_set($def_user_timezone);
 $date_current = date("YmdHi");
+$path_logs = $GLOBALS['dir_tmp'] . "ed.logs";
+
 
 // Funcao para esconder comentarios e comandos internos do ed.
 function f_hide($arg_a, $arg_b) {
     return preg_replace("#(\.ed\..*?\[\[).*?(\]\])#", "", $GLOBALS['content_terminal']);
+}
+
+// Funcao para gerar e salvar logs.
+function f_logs($log) {
+    $file_l = $GLOBALS['path_logs'];
+    $file_open = fopen($file_l, "a") or die("Unable to open file!");
+    fwrite($file_open, $log);
+    fclose($file_open);
 }
 
 // Funcao para visualizacao do arquivo texto.
@@ -18,8 +28,11 @@ function f_view($file_a, $file_b) {
     $file_open = fopen($file_v, "r") or die("Unable to open file!");
     $text_view = fread($file_open,filesize($file_v));
     fclose($file_open);
+    
+    $msg_log = ".ed.info.comment[[Open <" . $file_a . "> to view! (" . $GLOBALS['date_current'] . ")]]\n";
+    f_logs($msg_log);
 
-    return ".ed.info.comment[[Open <" . $file_a . "> to view! (" . $GLOBALS['date_current'] . ")]]\n" . $text_view;
+    return  $text_view;
 }
 
 // Funcao que executa os comandos internos.
@@ -40,39 +53,34 @@ function f_write($file_a, $file_b) {
     $file_open = fopen($file_w, "w") or die("Unable to open file!");
     fwrite($file_open, $GLOBALS['content_terminal']);
     fclose($file_open);
-
-    return ".ed.info.comment[[File <" . $file_a . "> wrote! (" . $GLOBALS['date_current'] . ")]]\n" . $GLOBALS['content_terminal'];
+    
+    $msg_log = ".ed.info.comment[[Wrote file <" . $file_a . "> ! (" . $GLOBALS['date_current'] . ")]]\n";
+    f_logs($msg_log);
 }
 
 
-$args = explode(" ", $cmd[1]);  //Captura argumentos.
-//if (count($args) == 3) {  //checa se existem 03 argumentos.
+// Menu de opcoes de argumentos e seu direcionamento
+$args = explode(" ", $cmd[1]);  //captura argumentos.
+switch ($args[0]) {
+    case "h":  //hide comments of the ed.
+        $result = f_hide($args[1], $args[2]);
+        break;
 
-    switch ($args[0]) {
-        case "h":  //hide comments of the ed.
-            $result = f_hide($args[1], $args[2]);
-            break;
+    case "r":  //run commands in file.
+        $result = f_run_internal($args[1], $args[2]);
+        break;
 
-        case "r":  //run commands in file.
-            $result = f_run_internal($args[1], $args[2]);
-            break;
-        
-        case "v":  //view file.
-            $result = f_view($args[1], $args[2]);
-            break;
+    case "v":  //view file.
+        $result = f_view($args[1], $args[2]);
+        break;
 
-        case "w":  //write file.
-            $result = f_write($args[1], $args[2]);
-            break;
-        
-        default:
-            $result = "Option not found!";
-    }
+    case "w":  //write file.
+        $result = f_write($args[1], $args[2]);
+        break;
 
-//}
-//else {
-//    $result = "Falta argumento";
-//}
+    default:
+        $result = "Option not found!";
+}
 
 return $result;
 
