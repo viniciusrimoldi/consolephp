b602365e18673358de75561efab228c4d5cc88dc
viniciusrimoldi <30911840+viniciusrimoldi@users.noreply.github.com>
2020-12-27 00:37:22 -0900

    Fixed bug return message function run.
    Change name commands:
    run --> exec (x)
    view --> read (r)

--- bin/ed.php
+++ bin/ed.php
@@ -8,6 +8,8 @@ $date_current = date("YmdHi");
 $path_logs = $GLOBALS['dir_tmp'] . "ed.logs";
 
 
+/* --------------------------- FUNCTIONS --------------------------- */
+
 // Funcao para esconder comentarios e comandos internos do ed.
 function f_hide($arg_a, $arg_b) {
     return preg_replace("#(\.ed\..*?\[\[).*?(\]\])#", "", $GLOBALS['content_terminal']);
@@ -22,7 +24,7 @@ function f_logs($log) {
 }
 
 // Funcao para visualizacao do arquivo texto.
-function f_view($file_a, $file_b) {
+function f_read($file_a, $file_b) {
     $file_v = $GLOBALS['dir_user'] . $file_a;
 
     $file_open = fopen($file_v, "r") or die("Unable to open file!");
@@ -30,20 +32,25 @@ function f_view($file_a, $file_b) {
     fclose($file_open);
     
     $msg_log = ".ed.info.comment[[Open <" . $file_a . "> to view! (" . $GLOBALS['date_current'] . ")]]\n";
-    f_logs($msg_log);
+    f_logs($msg_log); //write message logs.
 
-    return  $text_view;
+    return $text_view;
 }
 
 // Funcao que executa os comandos internos.
-function f_run_internal($arg_a, $arg_b) {
-    $msg_sucess = ".ed.info.comment[[Run internal commands! (" . $GLOBALS['date_current'] . ")]]\n";
+function f_exec_internal($arg_a, $arg_b) {
     if (preg_match("#\.ed\.run\.internal\[\[save\]\]#", $GLOBALS['content_terminal'])) {
         preg_match("#(\.ed\.file\.path\[\[).*?(\]\])#", $GLOBALS['content_terminal'], $find_path_name);
         $path_name_temp = explode('[[', $find_path_name[0]);
         $path_name_finish = explode(']]', $path_name_temp[1]);
-        return $msg_sucess . f_write($path_name_finish[0], 0);
+     
+        $result = f_write($path_name_finish[0], 0);
+     
+        $msg_log = ".ed.info.comment[[Run internal commands! (" . $GLOBALS['date_current'] . ")]]\n";
+        f_logs($msg_log); //write message logs.
     }
+
+    return $result;
 }
 
 // Funcao para escrever um arquivo.
@@ -53,31 +60,35 @@ function f_write($file_a, $file_b) {
     $file_open = fopen($file_w, "w") or die("Unable to open file!");
     fwrite($file_open, $GLOBALS['content_terminal']);
     fclose($file_open);
-    
-    $msg_log = ".ed.info.comment[[Wrote file <" . $file_a . "> ! (" . $GLOBALS['date_current'] . ")]]\n";
+
+    $msg_log = "\n.ed.info.comment[[Wrote file <" . $file_a . "> ! (" . $GLOBALS['date_current'] . ")]]";
     f_logs($msg_log);
+    
+    return $GLOBALS['content_terminal'];
 }
 
 
-// Menu de opcoes de argumentos e seu direcionamento
-$args = explode(" ", $cmd[1]);  //captura argumentos.
+/* --------------------------- MENU --------------------------- */
+
+// Menu de opcoes de argumentos e seu direcionamento.
+$args = explode(" ", $cmd[1]);  //Captura argumentos.
 switch ($args[0]) {
     case "h":  //hide comments of the ed.
         $result = f_hide($args[1], $args[2]);
         break;
 
-    case "r":  //run commands in file.
-        $result = f_run_internal($args[1], $args[2]);
+    case "x":  //exec/run commands in file.
+        $result = f_exec_internal($args[1], $args[2]);
         break;
-
-    case "v":  //view file.
-        $result = f_view($args[1], $args[2]);
+    
+    case "r":  //read/view file.
+        $result = f_read($args[1], $args[2]);
         break;
 
     case "w":  //write file.
         $result = f_write($args[1], $args[2]);
         break;
-
+    
     default:
         $result = "Option not found!";
 }
